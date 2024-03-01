70d0eb641659c99c2c1f82e587ff9464c3c03c0d
Zehra Demir <zdemir2702@gmail.com>
2024-03-01 15:18:06 +0300

    2.v

---
+++ src/test/java/HesapMakinesi.java
@@ -0,0 +1,34 @@
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class HesapMakinesi {
+
+    AndroidDriver<AndroidElement> driver;
+
+    @Test
+    public void hesapMakTest() throws MalformedURLException {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 2");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+       // capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\zehra\\Desktop\\Zehra\\IT\\Appium_01\\Apps\\Calculator_3.1.5_Apkpure.apk");
+
+        driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+
+    }
+
+
+
+}
